eaf1f81aa640bb1d9f97ff40260b713e0120aeb8
arshia-doshiri <arshiadsh@gmail.com>
2025-12-03 19:23:33 +0330

    add Ceremony part (( js , json , ((indext)) , style))

---
+++ js/hall.js
@@ -0,0 +1,56 @@
+// 1) گرفتن id از URL
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+// 2) خوندن دیتابیس halls.json
+fetch("../data/halls.json")
+  .then((res) => res.json())
+  .then((data) => {
+    // 3) پیدا کردن تالار موردنظر
+    const hall = data.find((item) => item.id == id);
+
+    // 4) پر کردن صفحه
+    if (hall) {
+      ////////////اسم مجموعه///////////////////////
+      document.getElementById("title").textContent = hall.title;
+      ////////////اسم مجموعه///////////////////////
+      document.getElementById("title_img").textContent = hall.title;
+      ////////////بازدید///////////////////////
+      document.getElementById("visit").textContent = hall.visit;
+      ////////////ضرفیت///////////////////////
+      document.getElementById(
+        "capacity"
+      ).textContent = `نفر : ${hall.capacity}`;
+      ////////////دیجی///////////////////////
+      document.getElementById("dj").textContent = hall.dj;
+      ////////////فیلم بردار///////////////////////
+      document.getElementById("videographer").textContent = hall.videographer;
+      ////////////سفره فضای باز///////////////////////
+      document.getElementById("outdoorTable").textContent = hall.outdoorTable;
+      ////////////سفره فضای داخل///////////////////////
+      document.getElementById("indoorTable").textContent = hall.indoorTable;
+      ////////////لوکیشن///////////////////////
+      document.getElementById("location").textContent = hall.location;
+
+      ///////make number persin style///////////
+      //...1
+      const price_number = document.querySelectorAll(".price-number");
+      price_number.forEach((mov) => {
+        const number = mov.textContent;
+
+        const iran_number = new Intl.NumberFormat("fa-IR").format(number);
+
+        mov.textContent = iran_number;
+      });
+
+      // price_number.forEach((mov) => {
+      //   if (mov.textContent === "ناعدد") {
+      //     console.log(` ${mov.textContent}:بی مقدار قیمت نداره  `);
+      //   } else {
+      //     console.log(` ${mov.textContent}:مقدار عددی`);
+      //   }
+      // });
+
+      //...2
+    } ////end
+  });
