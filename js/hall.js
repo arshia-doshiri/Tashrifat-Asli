287c1a9ec25b0bb4c3987f54653444ba20ef87ee
arshia-doshiri <arshiadsh@gmail.com>
2026-02-13 15:59:48 +0330

    image proplem on mobile ? fix
    is not show

--- js/hall.js
+++ js/hall.js
@@ -76,11 +76,19 @@ fetch("data/halls.json")
     document.querySelector(".photo").style.backgroundImage = hall.photo;
 
     // gallery
+    // document
+    //   .querySelectorAll(".gallery__item > .gallery-id")
+    //   .forEach((mov, i) => {
+    //     const gallery_id = hall[`gallery${i + 1}`];
+    //     mov.src = `http://127.0.0.1:5500/${gallery_id}`;
+    //   });
+
     document
       .querySelectorAll(".gallery__item > .gallery-id")
       .forEach((mov, i) => {
         const gallery_id = hall[`gallery${i + 1}`];
-        mov.src = `http://127.0.0.1:5500/${gallery_id}`;
+        // mov.src = `http://127.0.0.1:5500/${gallery_id}`;
+        mov.src = gallery_id;
       });
 
     ///adress bar
